b1c3f88c3c7e34fbefef45f88cdcd4fbc658bfff
Joshua M Smith <radjosh@gmail.com>
2024-09-11 19:22:01 +0000

    successfully making json from our map; now just to return it over http

--- go/src/main.go
+++ go/src/main.go
@@ -5,28 +5,18 @@ import (
 	"encoding/json"
 	"fmt"
 	"errors"
-	// "io"
+	"io"
 	"net/http"
 	"os"
 )
 
 import "dataProcessors"
 
-// func getRoot(w http.ResponseWriter, r *http.Request) {
-// 	fmt.Printf("got / request\n")
-// 	io.WriteString(w, "This is my website!\n")
-// }
-
-// func getHello(w http.ResponseWriter, r *http.Request) {
-// 	fmt.Printf("got /hello request")
-// 	io.WriteString(w, "Hello, HTTP!\n")
-// }
-
-func monsterPtrToJSON(m *dataProcessors.Monster) {
+func monsterPtrToJSON(m *dataProcessors.Monster) string {
 	var buffer bytes.Buffer
 	json.NewEncoder(&buffer).Encode(m)
 	fmt.Println("\nUsing Encoder:\n" + buffer.String())
-	// return buffer.String()
+	return buffer.String()
 }
 
 func getMonster(w http.ResponseWriter, r *http.Request) {
@@ -35,18 +25,11 @@ func getMonster(w http.ResponseWriter, r *http.Request) {
 	monsterName := r.URL.Query().Get("name")
 	monsterPtr := new(dataProcessors.Monster)
 	*monsterPtr = dataProcessors.Monsters[monsterName]
-	monsterPtrToJSON(monsterPtr)
-	// var payload = monsterPtrToJSON(monsterPtr)
-	// io.WriteString(w, payload)
-	// io.WriteString(w, payload)
-	// io.WriteString(w, monsterPtr.Alignment)
+	payload := monsterPtrToJSON(monsterPtr)
+	io.WriteString(w, payload)
 }
 
 func main() {
-	// fmt.Print("")
-	// fmt.Println(dataProcessors.Monsters["Dao"])
-	// http.HandleFunc("/", getRoot)
-	// http.HandleFunc("/hello", getHello)
 	http.HandleFunc("/monster", getMonster)
 	err := http.ListenAndServe(":3000", nil)
 	if errors.Is(err, http.ErrServerClosed) {
