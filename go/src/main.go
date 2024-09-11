3baa5db7acdb76e933a58d35f27a153cee781df8
Joshua M Smith <radjosh@gmail.com>
2024-09-11 17:57:51 +0000

    tutorial level PoC for http server

--- go/src/main.go
+++ go/src/main.go
@@ -1,8 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"errors"
+	"io"
+	"net/http"
+	"os"
+)
+
 import "dataProcessors"
+
+func getRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got / request\n")
+	io.WriteString(w, "This is my website!\n")
+}
+
+func getHello(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got /hello request")
+	io.WriteString(w, "Hello, HTTP!\n")
+}
+
 func main() {
 	// fmt.Print("")
-	fmt.Println(dataProcessors.Monsters["Dao"])
+	// fmt.Println(dataProcessors.Monsters["Dao"])
+	http.HandleFunc("/", getRoot)
+	http.HandleFunc("/hello", getHello)
+	err := http.ListenAndServe(":3000", nil)
+	if errors.Is(err, http.ErrServerClosed) {
+		fmt.Printf("server closed\n")
+	} else if err != nil {
+		fmt.Printf("error starting server: %s\n", err)
+		os.Exit(1)
+	}
 }
