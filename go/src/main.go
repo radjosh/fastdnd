f83890f949defaa8c46cc52b6a5a33e4c171cc0c
Joshua M Smith <radjosh@gmail.com>
2024-09-11 19:03:57 +0000

    successfully converting from GET/qs to json string

--- go/src/main.go
+++ go/src/main.go
@@ -1,30 +1,53 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"errors"
-	"io"
+	// "io"
 	"net/http"
 	"os"
 )
 
 import "dataProcessors"
 
-func getRoot(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("got / request\n")
-	io.WriteString(w, "This is my website!\n")
+// func getRoot(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Printf("got / request\n")
+// 	io.WriteString(w, "This is my website!\n")
+// }
+
+// func getHello(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Printf("got /hello request")
+// 	io.WriteString(w, "Hello, HTTP!\n")
+// }
+
+func monsterPtrToJSON(m *dataProcessors.Monster) {
+	var buffer bytes.Buffer
+	json.NewEncoder(&buffer).Encode(m)
+	fmt.Println("\nUsing Encoder:\n" + buffer.String())
+	// return buffer.String()
 }
 
-func getHello(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("got /hello request")
-	io.WriteString(w, "Hello, HTTP!\n")
+func getMonster(w http.ResponseWriter, r *http.Request) {
+	// ctx := r.Context()
+
+	monsterName := r.URL.Query().Get("name")
+	monsterPtr := new(dataProcessors.Monster)
+	*monsterPtr = dataProcessors.Monsters[monsterName]
+	monsterPtrToJSON(monsterPtr)
+	// var payload = monsterPtrToJSON(monsterPtr)
+	// io.WriteString(w, payload)
+	// io.WriteString(w, payload)
+	// io.WriteString(w, monsterPtr.Alignment)
 }
 
 func main() {
 	// fmt.Print("")
 	// fmt.Println(dataProcessors.Monsters["Dao"])
-	http.HandleFunc("/", getRoot)
-	http.HandleFunc("/hello", getHello)
+	// http.HandleFunc("/", getRoot)
+	// http.HandleFunc("/hello", getHello)
+	http.HandleFunc("/monster", getMonster)
 	err := http.ListenAndServe(":3000", nil)
 	if errors.Is(err, http.ErrServerClosed) {
 		fmt.Printf("server closed\n")
